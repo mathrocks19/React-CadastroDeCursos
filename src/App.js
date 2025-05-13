de06582e7d74713deb363d93129099d0ae60e318
ADS 2023.2N <36129382023.2N@senacrjedu.local>
2025-05-13 20:28:29 -0300

    feat: Configuracao inicial Hero

--- src/App.js
+++ src/App.js
@@ -3,26 +3,20 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import backgroundImage from './assets/background/imageOnce.png';
 import Header from './components/Header/Header';
-import './App.css'; 
+import Hero from './components/Hero/Hero';
+import './App.css';
 
 function App() {
   return (
-    <div className="App"> 
+    <div className="App">
       <Header />
 
       <section
         className="hero-section"
         style={{ backgroundImage: `url(${backgroundImage})` }}
       >
-        <div className="hero-content container text-center">
-          <h1>Bem-vindo ao Meu Site Incrível!</h1>
-          <p className="lead">
-            Descubra nossos produtos e serviços fantásticos.
-          </p>
-          <button className="btn btn-primary btn-lg mt-3">Saiba Mais</button>
-        </div>
+        <Hero />
       </section>
-
     </div>
   );
 }
---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <div className="hero-content container text-center">
+      <h1>ONCE HUMAN</h1>
+      <p className="lead">
+      O apocalipse não foi o fim. Foi uma transformação. Uma entidade alienígena, a "Grande Metamorfose", corrompeu o planeta, e os resquícios da humanidade lutam não apenas contra monstros grotescos, mas contra a própria mutação que os espreita. Para sobreviver, é preciso entender o que significa não ser mais... humano.
+      </p>
+      <button className="btn btn-primary btn-lg mt-3">Saiba Mais</button>
+    </div>
+  );
+};
+
+export default Hero;
